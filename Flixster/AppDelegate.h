6184e43d626d102d15f7024a7d88204b02ef15a9
Camryn Belcher <belchercd@fb.com>
2019-06-26 11:29:39 -0700

    Initial Commit

---
+++ Flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixster
+//
+//  Created by belchercd on 6/26/19.
+//  Copyright © 2019 belchercd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flixster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flixster
+//
+//  Created by belchercd on 6/26/19.
+//  Copyright © 2019 belchercd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
